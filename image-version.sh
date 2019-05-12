baa6f2563696a38cce55af0d982532b48ab2d7ce
Shoichiro Sakaigawa <s.sakaigawa@pumpkinheads.jp>
2019-05-12 02:33:18 +0100

    add version-label shell

---
+++ image-version.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "" >>/etc/motd
+echo "ImageVersion: TJBot Zero SD Card Image ${1}" >> /etc/motd
+echo "" >>/etc/motd
+echo "" >>/etc/motd
+echo "x((L|o_o| < Hello, TJBot." >>/etc/motd
+echo "" >>/etc/motd
+echo "" >>/boot/VERSION.txt
+echo "ImageVersion: TJBot Zero SD Card Image ${1}" >> /boot/VERSION.txt
+echo "" >>/boot/VERSION.txt
+echo "" >>/boot/VERSION.txt
+echo "x((L|o_o| < Hello, TJBot." >>/boot/VERSION.txt
+echo "" >>/boot/VERSION.txt
